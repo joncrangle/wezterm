fc6fc43aba85ce6771d218f57a2d4921ce93d7e8
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-03-24 20:34:44 -0400

    don't hard code colour

--- wezterm.lua
+++ wezterm.lua
@@ -32,7 +32,7 @@ config.cursor_blink_ease_out = 'Constant'
 --  Colour scheme and UI
 config.color_scheme = 'Tokyo Night'
 config.command_palette_bg_color = 'transparent'
-config.command_palette_fg_color = '#D6D6E6'
+config.command_palette_fg_color = config.resolved_palette.foreground
 config.cursor_thickness = 2
 config.default_cursor_style = 'BlinkingBar'
 config.force_reverse_video_cursor = true
