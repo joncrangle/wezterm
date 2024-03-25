bcd5002bc2dfc3dd9489a181fd24a319cdda7f02
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-03-24 22:16:32 -0400

    command bar style

--- wezterm.lua
+++ wezterm.lua
@@ -20,6 +20,9 @@ require('mouse').setup(config)
 require('links').setup(config)
 require('keys').setup(config)
 require('domains').setup(config)
+wezterm.plugin.require('https://gitlab.com/xarvex/presentation.wez').apply_to_config(config)
+--wezterm.plugins.require('https://github.com/joncrangle/which-key.wez').apply_to_config(config)
+--require('plugin.init').setup(config)
 
 -- Graphics config
 config.front_end = 'WebGpu'
@@ -31,8 +34,8 @@ config.cursor_blink_ease_out = 'Constant'
 
 --  Colour scheme and UI
 config.color_scheme = 'Tokyo Night'
-config.command_palette_bg_color = 'transparent'
-config.command_palette_fg_color = config.resolved_palette.foreground
+config.command_palette_bg_color = 'rgba(26, 27, 38, 0.92)'
+config.command_palette_fg_color = '#a9b1d6'
 config.cursor_thickness = 2
 config.default_cursor_style = 'BlinkingBar'
 config.force_reverse_video_cursor = true
