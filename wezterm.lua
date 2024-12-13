acb1006d04d60a30daf9c66a7960dd39b103b977
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-12-12 19:35:33 -0500

    Update Windows styling

--- wezterm.lua
+++ wezterm.lua
@@ -65,9 +65,9 @@ local secondary_font = 'IosevkaTerm Nerd Font'
 -- Windows, MacOS and Linux
 if wezterm.target_triple:find 'windows' then
   config.default_prog = { 'pwsh.exe', '-NoLogo' }
-  config.window_background_opacity = 0.4
+  config.window_background_opacity = 0
   config.window_decorations = 'RESIZE'
-  config.win32_system_backdrop = 'Acrylic'
+  config.win32_system_backdrop = 'Mica'
   font_size = 12
 elseif wezterm.target_triple:find 'linux' then
   config.window_decorations = 'NONE'
