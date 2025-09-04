829725a50d5e9d951c21992737699051d8b9efc7
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2025-09-03 21:36:37 -0400

    add ssh icon/color to tabline

--- tabline.lua
+++ tabline.lua
@@ -8,6 +8,7 @@ local process_to_icon = {
   ['lazysql'] = { wezterm.nerdfonts.cod_database, color = { fg = colors.ansi[4] } },
   ['nvim'] = { color = { fg = colors.compose_cursor } },
   ['ollama'] = { wezterm.nerdfonts.cod_code, color = { fg = colors.cursor_bg } },
+  ['ssh'] = { wezterm.nerdfonts.md_ssh, color = { fg = colors.ansi[3] } },
 }
 
 function M.tabline()
